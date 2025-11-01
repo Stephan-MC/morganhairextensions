dbda5ebaa06d96422f964f1b5ab90d628f309667
Minister <mundestephane13@gmail.com>
2025-11-01 04:59:33 +0100

    feat: Added the cart and the landing page

---
+++ projects/web/src/app/cart/cart.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+
+@Component({
+	selector: "web-cart",
+	imports: [RouterLink],
+	templateUrl: "./cart.page.ng.html",
+	styleUrl: "./cart.page.scss",
+})
+export class CartPage {}
