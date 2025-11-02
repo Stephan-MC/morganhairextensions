05f321919e188114f885fbe750d4fca646e2e688
Minister <mundestephane13@gmail.com>
2025-11-02 03:29:53 +0100

    feat: Updated routes

--- projects/web/src/app/app.routes.ts
+++ projects/web/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		pathMatch: "full",
+		loadComponent: () =>
+			import("./landing/landing.page").then((m) => m.LandingPage),
+	},
+	{
+		path: "cart",
+		loadComponent: () => import("./cart/cart.page").then((m) => m.CartPage),
+	},
+	{
+		path: "shop",
+		loadComponent: () => import("./shop/shop.page").then((m) => m.ShopPage),
+	},
+	{
+		path: "checkout",
+		loadComponent: () =>
+			import("./checkout/checkout.page").then((m) => m.CheckoutPage),
+	},
+];
