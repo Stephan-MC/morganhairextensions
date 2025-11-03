f4b202cd652e378061c0c6a987e685b908646ed1
Minister <mundestephane13@gmail.com>
2025-11-03 17:51:43 +0100

    feat: Enabled componentInputBinding to get resolved datas in to components as input

--- projects/web/src/app/app.config.ts
+++ projects/web/src/app/app.config.ts
@@ -1,13 +1,33 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+	ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
+	provideZonelessChangeDetection,
+} from "@angular/core";
+import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { environment } from "../environments/environment";
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+	provideHttpClient,
+	withFetch,
+	withInterceptors,
+} from "@angular/common/http";
+import {
+	provideClientHydration,
+	withEventReplay,
+} from "@angular/platform-browser";
+import { routes } from "./app.routes";
+import { ENVIRONMENT } from "shared";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+	providers: [
+		provideBrowserGlobalErrorListeners(),
+		provideZonelessChangeDetection(),
+		provideRouter(routes, withComponentInputBinding()),
+		provideClientHydration(withEventReplay()),
+		provideHttpClient(withFetch(), withInterceptors([])),
+		{
+			provide: ENVIRONMENT,
+			useValue: environment,
+		},
+	],
 };
