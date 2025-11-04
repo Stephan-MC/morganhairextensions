38ad5846398abfb409ce6c3365f81a9e0fdc4dc3
Minister <mundestephane13@gmail.com>
2025-11-04 20:06:43 +0100

    fix: Adjusted wrongly formed cannonical URLs

--- projects/web/src/app/app.ts
+++ projects/web/src/app/app.ts
@@ -1,6 +1,15 @@
-import { Component, inject, signal } from "@angular/core";
+import {
+	Component,
+	DOCUMENT,
+	effect,
+	Inject,
+	inject,
+	Renderer2,
+	signal,
+} from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import {
+	ActivatedRoute,
 	NavigationCancel,
 	NavigationEnd,
 	NavigationError,
@@ -15,6 +24,7 @@ import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { filter, map, of, switchMap, tap, timer } from "rxjs";
 import { Meta } from "@angular/platform-browser";
+import { environment } from "../environments/environment";
 
 @Component({
 	selector: "web-root",
@@ -23,7 +33,11 @@ import { Meta } from "@angular/platform-browser";
 	styleUrl: "./app.scss",
 })
 export class App {
+	private _meta = inject(Meta);
+	private _renderer = inject(Renderer2);
+	private _document = inject(DOCUMENT);
 	private _router = inject(Router);
+	private _route = inject(ActivatedRoute);
 	protected readonly title = signal("web");
 
 	loading = toSignal(
@@ -50,11 +64,35 @@ export class App {
 		},
 	);
 
-	constructor(meta: Meta) {
-		meta.updateTag({
-			id: "og:url",
-			property: "og:url",
-			content: "https://morganhairextensions.com" + this._router.url,
-		});
+	constructor() {
+		this._router.events
+			.pipe(
+				filter((e) => e instanceof NavigationEnd),
+				map(() => `${environment.url.base}${this._router.url.split("?")[0]}`),
+
+				// Set canonical
+				tap((url) => {
+					const element = this._document.querySelector("link[rel='canonical']");
+
+					if (element) {
+						this._renderer.removeChild(this._document.head, element);
+					}
+
+					const link: HTMLLinkElement = this._renderer.createElement("link");
+					this._renderer.setAttribute(link, "rel", "canonical");
+					this._renderer.setAttribute(link, "href", url);
+					this._renderer.appendChild(this._document.head, link);
+				}),
+
+				// Update meta tags
+				tap((url) => {
+					this._meta.updateTag({
+						id: "og:url",
+						property: "og:url",
+						content: url,
+					});
+				}),
+			)
+			.subscribe();
 	}
 }
